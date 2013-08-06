10742306d9b80e6876415be3578feefbc1ca1d3a
xrow <bjoern@xrow.de>
2013-08-06 16:10:41 +0200

    bugfixing

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,52 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/xrowworkflow</name>
+    <message>
+        <source>Workflow: select a publication date in the future.</source>
+        <translation>Workflow: wählen Sie ein Veröffentlichungsdatum, welches in der Zukunft liegt.</translation>
+    </message>
+    <message>
+        <source>Workflow: select an expiry date in the future.</source>
+        <translation>Workflow: wählen Sie ein Ablaufdatum, welches in der Zukunft liegt.</translation>
+    </message>
+    <message>
+        <source>Workflow</source>
+        <translation>Workflow</translation>
+    </message>
+    <message>
+        <source>Date of publication</source>
+        <translation>Veröffentlichungsdatum</translation>
+    </message>
+    <message>
+        <source>Date of expiry</source>
+        <translation>Ablaufdatum</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Zeit</translation>
+    </message>
+    <message>
+        <source>offline</source>
+        <translation>offline</translation>
+    </message>
+    <message>
+        <source>move to</source>
+        <translation>offline und verschieben nach</translation>
+    </message>
+    <message>
+        <source>delete/source>
+        <translation>löschen</translation>
+    </message>
+    <message>
+        <source>delete/source>
+        <translation>löschen</translation>
+    </message>
+    <message>
+        <source>Error: you can not move a node in itself</source>
+        <translation>Fehler: Sie können einen Knoten nicht in sich selber verschieben.</translation>
+    </message>
+</context>
